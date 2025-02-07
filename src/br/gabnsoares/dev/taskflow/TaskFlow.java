23d9896a7d0f15ad0dff88401df65601f0b1723c
Gabriel Soares <120689729+GabrielnSoares@users.noreply.github.com>
2025-02-06 22:11:21 -0300

    TaskFlow gestão de tarefas

---
+++ src/br/gabnsoares/dev/taskflow/TaskFlow.java
@@ -0,0 +1,10 @@
+package br.gabnsoares.dev.taskflow;
+
+public class TaskFlow {
+
+    public static void main(String[] args) {
+
+    }
+
+    // criando planejamento do sistema de gestao de tarefas
+}
